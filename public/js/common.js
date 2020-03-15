dd3f2e6c0d0e4034bcc03a42bbf3c77616d3cc7a
Aleksandr3012 <flaming541@gmail.com>
2020-03-15 23:39:06 +0200

    changes from 15.03

--- public/js/common.js
+++ public/js/common.js
@@ -27,7 +27,7 @@ function eventHandler() {
 	JSCCommon.inputMask(); // JSCCommon.CustomInputFile();
 	// добавляет подложку для pixel perfect
 
-	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/index-new.png);"></div>'); // /добавляет подложку для pixel perfect
+	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/login.png);"></div>'); // /добавляет подложку для pixel perfect
 	// /закрыть/открыть мобильное меню
 
 	function heightses() {
--- sourse/js/common.js
+++ sourse/js/common.js
@@ -25,7 +25,7 @@ function eventHandler() {
 
 	// JSCCommon.CustomInputFile();
 	// добавляет подложку для pixel perfect
-	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/index-new.png);"></div>')
+	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/login.png);"></div>')
 	// /добавляет подложку для pixel perfect
 
 
